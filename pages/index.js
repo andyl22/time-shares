25365097665f12bfa28cdad681a97438e08b818e
Andy Lau <landywl4@gmail.com>
2022-08-01 20:10:12 -0700

    Added image fetching from unsplash

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
+  const [pics, setPics] = useState();
+  const accessKey = 'y7CUOcVNwrD1FoUXE8VXn-1a_6NnbnKCmyQ7L6ICoJE';
+  const secret = '6qsnSRFklAVoBO2egFFRhhRS8JSOYHPAlofmggnDxVI';
+
+  const getData = async () =>
+    fetch(
+      `https://api.unsplash.com/search/photos?query=pets&client_id=${accessKey}`
+    )
+      .then((res) => res.json())
+      .then((res) => res.results[0])
+      .then((res) => setPics(<img src={res.urls.small} />));
+
   return (
     <>
       <Head>
@@ -14,7 +27,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        
+        <button onClick={getData}>Click Me</button>
+        {pics}
       </main>
     </>
   );
