669de1ab406652a63f72ce2aed10f166af13fa11
Andy <landywl4@gmail.com>
2022-08-03 14:31:56 -0700

    Modified item display zoom

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.scss';
+import Image from 'next/image';
 
 export function getStaticProps() {
   return {
@@ -20,11 +21,13 @@ export default function Home(props) {
   useEffect(() => {
     const getData = async () =>
       fetch(
-        `https://api.unsplash.com/search/photos?query=pets&client_id=${accessKey}`
+        `https://api.unsplash.com/search/photos?query=pets&orientation=squarish&client_id=${accessKey}`
       )
         .then((res) => res.json())
         .then((res) => res.results[Math.floor(Math.random() * 10)])
-        .then((res) => setPics(<img src={res.urls.small} />));
+        .then((res) =>
+          setPics(<Image src={res.urls.small} width="300" height="300" />)
+        );
     getData();
   }, []);
 
