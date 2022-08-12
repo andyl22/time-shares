f7298f7cb14978b6ad49d443b2924a08e370da9d
Andy <landywl4@gmail.com>
2022-08-12 14:42:40 -0700

    Calendar interval and styling changes

--- components/Calendar/CalendarDay.jsx
+++ components/Calendar/CalendarDay.jsx
@@ -37,7 +37,10 @@ export default function CalendarDay(props) {
     );
   };
 
-  const hours = [0, 3, 6, 9, 12, 15, 18, 21, 24];
+  const hours = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24
+  ];
   const mappedHours = hours.map((time) => (
     <li
       className={`${styles.timeUnit} ${
