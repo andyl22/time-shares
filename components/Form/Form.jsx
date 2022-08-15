aa85cf1e3b4a4b33da60e35891516492d90cc7a4
Andy <landywl4@gmail.com>
2022-08-15 13:44:31 -0700

    Added form component for overall style and labels for new create object for time shares dialog

---
+++ components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import styles from './Form.module.scss';
+import PropTypes from 'prop-types';
+
+export default function Form({ children }) {
+  return <div className={styles.formContainer}>{children}</div>;
+}
+
+Form.propTypes = {
+  children: PropTypes.element
+};
--- pages/browse/index.js
+++ pages/browse/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import { useState } from 'react';
+import Dialog from '../../components/Dialog/Dialog';
 import FilterControlledSection from '../../components/FilterControl/FilterControlledSection';
+import Form from '../../components/Form/Form';
 import styles from './browse.module.scss';
 
 export async function getStaticProps() {
@@ -14,6 +17,29 @@ export async function getStaticProps() {
 
 export default function browse(props) {
   const { entityData } = props;
+  const [showDialog, setShowDialog] = useState(false);
+
+  const toggleDialog = () => {
+    setShowDialog(!showDialog);
+  };
+
+  const addObjectForm = (
+    <Form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor="description">Description</label>
+        <input type="text" id="description" />
+      </div>
+      <div>
+        <label htmlFor="rate">Rate</label>
+        <input type="text" id="rate" />
+      </div>
+    </Form>
+  );
+
   return (
     <>
       <Head>
@@ -25,7 +51,16 @@ export default function browse(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.browsePage}>
+        <button onClick={toggleDialog}>Create a new booking</button>
         <FilterControlledSection entityData={entityData} />
+        <Dialog
+          show={showDialog}
+          dialogHeader={'Add Object'}
+          dialogContent={addObjectForm}
+          hideDialog={toggleDialog}
+          dialogConfirmAction={() => {}}
+          dialogCancelAction={() => {}}
+        />
       </main>
     </>
   );
