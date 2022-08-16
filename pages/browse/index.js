8502654c29a6d034d238a3f58e0b31f548096749
Andy <landywl4@gmail.com>
2022-08-15 17:02:34 -0700

    Moved form from page to component for entity creation

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useState } from 'react';
 import Dialog from '../../components/Dialog/Dialog';
 import FilterControlledSection from '../../components/FilterControl/FilterControlledSection';
-import Form from '../../components/Form/Form';
+import FormAddEntity from '../../components/Form/FormAddEntity';
 import styles from './browse.module.scss';
 
 export async function getStaticProps() {
@@ -23,23 +23,6 @@ export default function browse(props) {
     setShowDialog(!showDialog);
   };
 
-  const addObjectForm = (
-    <Form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="description">Description</label>
-        <input type="text" id="description" />
-      </div>
-      <div>
-        <label htmlFor="rate">Rate</label>
-        <input type="text" id="rate" />
-      </div>
-    </Form>
-  );
-
   return (
     <>
       <Head>
@@ -56,7 +39,7 @@ export default function browse(props) {
         <Dialog
           show={showDialog}
           dialogHeader={'Add Object'}
-          dialogContent={addObjectForm}
+          dialogContent={<FormAddEntity />}
           hideDialog={toggleDialog}
           dialogConfirmAction={() => {}}
           dialogCancelAction={() => {}}
