7b0128e1446af7b18386ba54b700a6921087d031
Andy <landywl4@gmail.com>
2022-11-16 10:34:59 -0800

    Updated package and next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+// eslint-disable-next-line no-undef
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? 'https://andyl22.github.io/Grid/' : undefined,
   images: {
     domains: ['images.unsplash.com', 'localhost']
   }
 };
 
+// eslint-disable-next-line no-undef
 module.exports = nextConfig;
